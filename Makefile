ba1b029f62ed976ce03cfaaac241b2d06d1b6f24
HelioLucio <helio.lucio.jr@hotmail.com>
2024-04-08 15:21:41 -0300

    Add unit tests for UI module

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 CC=gcc
 CFLAGS=-Iinclude
+LDFLAGS = -lcunit
 DEPS = include/ui.h
 OBJ = src/ui.o src/main.o
+TEST_SRC = tests/test_ui.c
+TEST_OBJ = $(TEST_SRC:.c=.o)
+TEST_EXEC = test_ui
 
 %.o: %.c $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
@@ -9,7 +13,16 @@ OBJ = src/ui.o src/main.o
 todo_list: $(OBJ)
 	$(CC) -o $@ $^ $(CFLAGS)
 
-.PHONY: clean
+$(TEST_OBJ): $(TEST_SRC)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+$(TEST_EXEC): src/ui.o $(TEST_OBJ)
+	$(CC) -o $@ $^ $(CFLAGS) $(LDFLAGS)
+
+.PHONY: clean test
+
+test: $(TEST_EXEC)
+	./$(TEST_EXEC)
 
 clean:
-	rm -f $(OBJ) todo_list
+	rm -f $(OBJ) todo_list $(TEST_OBJ) $(TEST_EXEC)
---
+++ tests/test_ui.c
@@ -0,0 +1,36 @@
+#include "CUnit/Basic.h"
+#include "ui.h"
+
+void test_remove_newline(void) {
+    char str[] = "Hello, World!\n";
+    remove_newline(str);
+    CU_ASSERT_STRING_EQUAL(str, "Hello, World!");
+}
+
+int main() {
+    CU_pSuite pSuite = NULL;
+
+    /* initialize the CUnit test registry */
+    if (CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+    /* add a suite to the registry */
+    pSuite = CU_add_suite("Suite_1", NULL, NULL);
+    if (NULL == pSuite) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* add the tests to the suite */
+    if ((NULL == CU_add_test(pSuite, "test of remove_newline()", test_remove_newline)))
+    {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* Run all tests using the CUnit Basic interface */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
